2e80c1edeae6125901711ec0845c3b645a94f3a9
zdw <1099509472@qq.com>
2022-11-18 16:10:15 +0800

    2022/11/18

--- day06/com.atguigu.java/src/BubbleSort.java
+++ day06/com.atguigu.java/src/BubbleSort.java
@@ -6,7 +6,19 @@ public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = new int[]{43,32,76,-98,0,64,33,-21,32,99};
 /***************************************************************************/
-        //冒泡排序
+        for(int i = 0; i < arr.length - 1; i++) {
+            for(int j = 0; j < arr.length - i - 1; j++){
+                if(arr[j] > arr[j + 1]) {//如果第一个比第二个大，那就交换，使得第一个比第二个小
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+
+//冒泡排序
+        /*
         for(int i = 0; i < arr.length - 1; i++) {//循环条件是冒泡排序的关键
             for(int j = 0; j < arr.length - 1 - i; j++) {
                 if(arr[j] > arr[j + 1]) {
@@ -17,11 +29,11 @@ public class BubbleSort {
                 }
             }
         }
-
+*/
 
         //遍历显示数组状态
-        for (int j : arr) {
-            System.out.println("数组当前为：" + j);
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "  ");
         }
 
     }
